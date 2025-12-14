2ebd0adfbb3f6e7fc01746f2b7362e2339b03c20
Ken Moini <ken@kenmoini.com>
2025-12-14 02:20:33 -0500

    test common doc fragments

---
+++ kenmoini/unifi_network/plugins/doc_fragments/common.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2025, Ken Moini <ken@kenmoini.com>
+# MIT License
+
+# Options for authenticating with the API.
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+    DOCUMENTATION = r"""
+options:
+  unifi_network_url:
+    description:
+      - This is the URL of your Unifi Network endpoint
+    required: true
+    aliases: ['unifi_network_api', 'url']
+    type: str
+    env:
+      - name: UNIFI_NETWORK_URL
+      - name: UNIFI_NETWORK_API
+  unifi_network_api_key:
+    description:
+      - This is the API Key for your Unifi Network endpoint
+    required: true
+    aliases: ['api_key']
+    type: str
+    env:
+      - name: UNIFI_NETWORK_API_KEY
+  unifi_network_skip_tls_verify:
+    description:
+      - Whether or not to skip TLS verification
+    required: false
+    default: false
+    aliases: ['skip_tls_verify']
+    type: bool
+    env:
+      - name: UNIFI_NETWORK_SKIP_TLS_VERIFY
+"""
--- kenmoini/unifi_network/plugins/modules/acl_rules_info.py
+++ kenmoini/unifi_network/plugins/modules/acl_rules_info.py
@@ -17,33 +17,10 @@ version_added: "1.0.0"
 description:
     - Retrieves information about the list of ACL Rules managed by this Unifi Network Application.
 
+extends_documentation_fragment:
+  - kenmoini.unifi_network.common
+
 options:
-  unifi_network_url:
-    description:
-      - This is the URL of your Unifi Network endpoint
-    required: true
-    aliases: ['unifi_network_api', 'url']
-    type: str
-    env:
-      - name: UNIFI_NETWORK_URL
-      - name: UNIFI_NETWORK_API
-  unifi_network_api_key:
-    description:
-      - This is the API Key for your Unifi Network endpoint
-    required: true
-    aliases: ['api_key']
-    type: str
-    env:
-      - name: UNIFI_NETWORK_API_KEY
-  unifi_network_skip_tls_verify:
-    description:
-      - Whether or not to skip TLS verification
-    required: false
-    default: false
-    aliases: ['skip_tls_verify']
-    type: bool
-    env:
-      - name: UNIFI_NETWORK_SKIP_TLS_VERIFY
   unifi_network_site_id:
     description:
       - The Site UUID to query for networks.
